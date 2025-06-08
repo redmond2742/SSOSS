6372d061f99301c82069d37a158e6bcab21e018f
Matt Redmond <10541289+redmond2742@users.noreply.github.com>
2025-06-07 22:49:34 -0700

    Handle optional icecream dependency

--- src/ssoss/__init__.py
+++ src/ssoss/__init__.py
@@ -4,8 +4,13 @@ from ssoss.process_road_objects import *
 from ssoss.process_video import *
 from ssoss.static_road_object import *
 import importlib.metadata
-from icecream import install
-install()
+try:
+    from icecream import install
+    install()
+except Exception:
+    # icecream is an optional dependency used for debugging. Avoid failing
+    # if it isn't installed when running in minimal environments such as tests.
+    pass
 
 __version__ = importlib.metadata.version("ssoss")
 
